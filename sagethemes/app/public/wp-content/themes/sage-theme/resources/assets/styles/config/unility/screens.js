fe8394227cd87ae3d796982599f97ebccd956fe7
cntruong7701 <cntruong12c5nct@gmail.com>
2022-12-05 22:51:55 +0700

    my commit

---
+++ sagethemes/app/public/wp-content/themes/sage-theme/resources/assets/styles/config/unility/screens.js
@@ -0,0 +1,14 @@
+const Screens = {
+  'md': '481px',
+  'lg': '768px',
+  'xl': '1024px',
+  '2xl': '1440px',
+  '2k': '2000px',
+  'down_xl': {'max': '1199px'},
+  'down_lg': {'max': '991px'},
+  'down_md': {'max': '767px'},
+  'down_sm': {'max': '480px'}
+}
+module.exports = {
+  Screens
+}
---
+++ sagethemes/app/public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-cao_nhat_truong.blade.php
@@ -0,0 +1,50 @@
+<section class="wrap xl:w-4/5 xl:flex xl:justify-center mx-auto lg:flex-none">
+    <div class="title xl:w-2/5 lg:w-full md:w-[88%] md:mx-[auto]">
+        <div class="Line-4"></div>
+        <h2 class="title-text text-[36] lg:w-full">
+            The Encompass Platform Seamlessly Connects the Supply Chain,
+            Allowing You to:
+        </h2>
+    </div>
+    <div class="info xl:w-3/5 xl:flex lg:flex lg:w-full md:flex-none">
+        <div class="column1 mr-2.5 xl:w-1/2 lg:w-1/2 md:w-3/4 md:mx-[auto]">
+            <div class="xl:w-4/5 ">
+                <h3 class="text-[24px]">Drive Business Growth</h3>
+                <p class="font-[300] text-[18px]">
+                    Tap into the power of predictive analytics and demand forecasting.
+                    Capitalize on revenue-generating opportunities faster. Capture mission-critical data
+                    across your entire operation—including all three supply chain tiers—empowering you to
+                    bring your beverage or food business into new levels of profitability.
+                </p>
+            </div>
+            <div class="xl:w-4/5 ">
+                <h3 class="text-[24px]">Improve Operational Efficiencies</h3>
+                <p class="font-[300] text-[18px]">
+                    Streamline processes and save money by reducing leakage with a fully
+                    integrated ERP system that works for your business. Encompass ERP makes it possible to
+                    identify where, when, and how resources can be best allocated to provide the most
+                    optimal outcome.
+                </p>
+            </div>
+        </div>
+        <div class="column2 xl:w-1/2 lg:w-1/2 md:w-3/4 md:mx-[auto]">
+            <div class="xl:w-4/5">
+                <h3 class="text-[24px]">Leverage Real-Time Data for Better Decision-Making</h3>
+                <p class="font-[300] text-[18px]">
+                    Accelerate business decisions with better, more accurate
+                    system-to-system data and comprehensive reporting that results in deep, actionable
+                    insights. Stay ahead of potential disruptions and maintain a finger on the pulse of your
+                    supply chain from anywhere in the world.
+                </p>
+            </div>
+            <div class="xl:w-4/5">
+                <h3 class="text-[24px]">Enhance Team Dynamics & Employee Satisfaction</h3>
+                <p class="font-[300] text-[18px]">
+                    When your labor force has the tools they need to succeed, the workplace
+                    runs like a well-oiled machine. With Encompass ERP, every stakeholder across your supply
+                    chain—from purchasing to Point-of-Sale—has what they need to do their jobs effectively.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
