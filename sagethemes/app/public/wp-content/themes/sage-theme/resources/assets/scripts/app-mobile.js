a451a4b85fb231837f4b28c1514ceda696e564f1
cntruong7701 <cntruong12c5nct@gmail.com>
2022-12-06 21:05:21 +0700

    update

--- sagethemes/app/public/wp-content/themes/sage-theme/resources/assets/scripts/app-mobile.js
+++ sagethemes/app/public/wp-content/themes/sage-theme/resources/assets/scripts/app-mobile.js
@@ -10,5 +10,6 @@ import './modules/Header';
 import './modules/Menu';
 import './modules/Popup';
 import './modules/SelectC8';
+import './modules/slick'
 
 console.log('App Mobile')
---
+++ sagethemes/app/public/wp-content/themes/sage-theme/resources/views/template-parts/page/module-module_slider.blade.php
@@ -0,0 +1,28 @@
+<section class="Slick-slider">
+    <div class="carousel">
+        <div>
+            <h2>
+                “We’ve done more with Encompass in the past 6 months than we did in 3 years on our previous solution.”
+            </h2>
+            <p>David Little CTO, Savannah Dis</p>
+        </div>
+        <div>
+            <h2>
+                “We’ve done more with Encompass in the past 6 months than we did in 3 years on our previous solution.”
+            </h2>
+            <p>David Little CTO, Savannah Dis</p>
+        </div>
+        <div>
+            <h2>
+                “We’ve done more with Encompass in the past 6 months than we did in 3 years on our previous solution.”
+            </h2>
+            <p>David Little CTO, Savannah Dis</p>
+        </div>
+        <div>
+            <h2>
+                “We’ve done more with Encompass in the past 6 months than we did in 3 years on our previous solution.”
+            </h2>
+            <p>David Little CTO, Savannah Dis</p>
+        </div>
+    </div>
+</section>
